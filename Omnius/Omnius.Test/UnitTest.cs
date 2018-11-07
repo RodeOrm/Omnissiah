721031d5b371154a341c137a5eaa35647bd3290d
RodeOrm <ilyin-a-l@yandex.ru>
2018-11-07 12:50:36 +0300

    Р”РѕР±Р°РІРёР» РїСЂРѕРµРєС‚ С‚РµСЃС‚РёСЂРѕРІР°РЅРёСЏ Рё РґРІР° С‚РµСЃС‚Р°

---
+++ Omnius/Omnius.Test/UnitTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Omnius.UI.Models;
+
+namespace Omnius.Test
+{
+    [TestClass]
+    public class PageViewModelTest
+    {
+        private PageViewModel _pageViewModel;
+
+        [TestMethod]
+        public void HasPreviousPage()
+        {
+            // Arrange 
+            _pageViewModel = new PageViewModel(100,1,7,null);
+
+            // Act
+            var result = _pageViewModel.HasPreviousPage;
+
+            //Assert
+            Assert.IsFalse(result, "Не работает проверка наличия предыдущей страницы");
+        }
+
+        [TestMethod]
+        public void HasNextPage()
+        {
+            // Arrange 
+            _pageViewModel = new PageViewModel(100, 1, 7, null);
+
+            // Act
+            var result = _pageViewModel.HasNextPage;
+
+            //Assert
+            Assert.IsTrue(result, "Не работает проверка наличия следующей страницы");
+        }
+    }
+}
